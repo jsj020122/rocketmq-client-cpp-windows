99897485122775945efac8fd77091e93ed0b0561
jsj020122 <jsj020100@163.com>
2018-11-20 17:24:42 +0800

    add dependency lib

--- win32_build.bat
+++ win32_build.bat
@@ -6,5 +6,6 @@ set  ZLIB_SOURCE="%cd%\zlib-1.2.3-src\src\zlib\1.2.3\zlib-1.2.3\"
 cd boost_1_58_0
 call bootstrap.bat
 @echo build start.....
-bjam.exe --with-iostreams --with-system --with-thread  --with-chrono --with-filesystem  --build-type=complete
+bjam.exe --with-serialization --with-atomic --with-log --with-locale --with-iostreams --with-system --with-regex --with-thread --with-date_time --with-chrono --with-filesystem  link=static  threading=multi variant=release runtime-link=shared
 cd ../../
+devenv ./rocketmq-client-cpp.sln  /Rebuild "Release|x86" /out log.txt
